433d209e63c21f9ce4beabb32f33fbca73528d92
Shihab Hasan <shihabbd018@gmail.com>
2023-09-14 23:36:52 +0600

    render individual course card to the UI

--- src/components/courses/Course.jsx
+++ src/components/courses/Course.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Icons from '../SVG/Icons';
 
 const Course = ({ course }) => {
-    return (
-        <div>Course</div>
-    )
+    const { cover_img, title, description, price, credit_hour } = course;
+    return <div className='p-4 bg-white rounded-xl'>
+        <div>
+            <img src={cover_img} alt={`cover image for ${title}`} />
+        </div>
+
+        <div>
+            <h3 className='text-base font-semibold  my-4'>{title}</h3>
+
+            <p className='text-sm text-[#1C1B1B99] '> {description} </p>
+
+            <Icons credit_hour={credit_hour} price={price} />
+
+            <button className="w-full py-2 bg-[#2F80ED] text-white font-semibold text-lg rounded-lg mt-7">Select</button>
+
+        </div>
+
+    </div>
 }
 
 export default Course
--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -15,10 +15,10 @@ const Courses = () => {
             .then(data => setCourses(data))
     }, []);
 
-
+    console.log(courses)
 
     return (
-        <section className='flex-1'>
+        <section className='flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {
                 courses.map(course => <Course key={course.id} course={course} />)
             }
